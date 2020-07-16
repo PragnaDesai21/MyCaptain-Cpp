d270d9a3eb00dce1a68d655d05bae92087ea58b7
PragnaDesai21 <68388276+PragnaDesai21@users.noreply.github.com>
2020-07-16 21:03:54 +0530

    Add files via upload

---
+++ prodof2nums.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+   float n1,n2,p;
+   cout<<"enter first number"<<endl;
+   cin>>n1;
+   cout<<"enter second number"<<endl;
+   cin>>n2;
+   p=n1*n2;
+   cout<<"product of the numbers is"<< p <<endl;
+}
+
